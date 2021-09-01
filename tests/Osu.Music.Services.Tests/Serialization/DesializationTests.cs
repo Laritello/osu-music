c177fd09ed09dbe12df8cb47b4676eac5f79975f
Laritello <larik97@gmail.com>
2021-09-02 00:00:32 +0300

    Commented out unused tests

--- tests/Osu.Music.Services.Tests/Serialization/DesializationTests.cs
+++ tests/Osu.Music.Services.Tests/Serialization/DesializationTests.cs
@@ -1,6 +1,4 @@
 ﻿using NUnit.Framework;
-using Osu.Music.Services.IO;
-using Osu.Music.Services.Serialization;
 using System.IO;
 
 namespace Osu.Music.Services.Tests.Serialization
@@ -14,11 +12,11 @@ namespace Osu.Music.Services.Tests.Serialization
             // Set up logic
         }
 
-        [Test]
-        public void DeserializeTest()
-        {
-            var beatmap = BeatmapConverter.Deserialize("Resources\\YouSayRun.osu");
-            Assert.AreEqual(beatmap, beatmap, "Not equalt");
-        }
+        //[Test]
+        //public void DeserializeTest()
+        //{
+        //    var beatmap = BeatmapConverter.Deserialize("Resources\\YouSayRun.osu");
+        //    Assert.AreEqual(beatmap, beatmap, "Not equalt");
+        //}
     }
 }
