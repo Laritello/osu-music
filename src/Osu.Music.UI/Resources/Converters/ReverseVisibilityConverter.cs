8e2748bba0610e6d583e0c05297073e870877251
Laritello <larik97@gmail.com>
2021-09-02 04:00:49 +0300

    Custom TitleBar

---
+++ src/Osu.Music.UI/Resources/Converters/ReverseVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Osu.Music.UI.Resources.Converters
+{
+    public class ReverseVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+
+            return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
