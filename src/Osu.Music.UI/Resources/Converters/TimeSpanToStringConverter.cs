ed04c56cf056db1b2b95f19b6ef585cf4f3948cb
Laritello <larik97@gmail.com>
2021-09-04 21:09:11 +0300

    Styles refactoring

--- src/Osu.Music.UI/Resources/Converters/TimeSpanToStringConverter.cs
+++ src/Osu.Music.UI/Resources/Converters/TimeSpanToStringConverter.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows.Data;
 
 namespace Osu.Music.UI.Resources.Converters
 {
-    class TimeSpanToStringConverter : IValueConverter
+    public class TimeSpanToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
