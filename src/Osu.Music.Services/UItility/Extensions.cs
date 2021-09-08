2dc2828ec9cbe1f257201eb861920b0cb8f9048c
Laritello <larik97@gmail.com>
2021-09-08 23:54:04 +0300

    Extensions for color convertation added

---
+++ src/Osu.Music.Services/UItility/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace Osu.Music.Services.UItility
+{
+    public static class Extensions
+    {
+        public static string ToHex(this Color color) => color.ToString();
+
+        public static Color FromHex(this string str) => (Color)ColorConverter.ConvertFromString(str);
+    }
+}
