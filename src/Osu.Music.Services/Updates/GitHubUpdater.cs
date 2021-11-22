af491694f20585f347b0c1d65a71460bc443eb50
Laritello <larik97@gmail.com>
2021-11-22 21:24:46 +0300

    Added wrapper for UpdateManager

---
+++ src/Osu.Music.Services/Updates/GitHubUpdater.cs
@@ -0,0 +1,44 @@
+﻿using Osu.Music.Common.Enums;
+using Prism.Mvvm;
+using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osu.Music.Services.Updates
+{
+    /// <summary>
+    /// Wrapper for Squirrel UpdateManager
+    /// </summary>
+    public class GitHubUpdater : BindableBase
+    {
+        private UpdateManager _manager;
+        public UpdateManager Manager
+        {
+            get => _manager;
+            set => SetProperty(ref _manager, value);
+        }
+
+        private UpdateState _state;
+        public UpdateState State
+        {
+            get => _state;
+            set => SetProperty(ref _state, value);
+        }
+
+        public GitHubUpdater()
+        {
+            State = UpdateState.Latest;
+        }
+
+        public async void CheckForUpdates()
+        {
+            if (Manager != null)
+            {
+                var info = await Manager.CheckForUpdate();
+                State = info.ReleasesToApply.Count > 0 ? UpdateState.Available : UpdateState.Latest;
+            }
+        }
+    }
+}
--- src/Osu.Music/ViewModels/MainWindowViewModel.cs
+++ src/Osu.Music/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Prism.Mvvm;
+﻿using Osu.Music.Services.Updates;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace Osu.Music.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        private GitHubUpdater _updater;
+        public GitHubUpdater Updater
+        {
+            get => _updater;
+            set => SetProperty(ref _updater, value);
+        }
+
         private string _title = "osu.Music";
         public string Title
         {
