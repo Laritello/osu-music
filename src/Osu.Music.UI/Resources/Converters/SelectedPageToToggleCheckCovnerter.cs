db117200fff3723673bbd955d4f82138c16c41ab
Laritello <larik97@gmail.com>
2022-06-11 01:37:51 +0300

    Fixes #47 Binded IsChecked to VM property
    IsChecked is now binded to the MainViewModel's SelectedPage property.

---
+++ src/Osu.Music.UI/Resources/Converters/SelectedPageToToggleCheckCovnerter.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Osu.Music.UI.Resources.Converters
+{
+    public class SelectedPageToToggleCheckCovnerter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            if (!(value is BindableBase))
+                throw new ArgumentException("Input argument must be of type BindableBase");
+
+            var type = value.GetType().Name;
+            var name = parameter as string;
+            return type == name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
