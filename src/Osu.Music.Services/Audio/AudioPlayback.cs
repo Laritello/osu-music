7163294f051462a168bed720a19e8611c58fbd6a
Laritello <larik97@gmail.com>
2022-06-26 20:57:53 +0300

    Implemented Player class

--- src/Osu.Music.Services/Audio/AudioPlayback.cs
+++ src/Osu.Music.Services/Audio/AudioPlayback.cs
@@ -10,68 +10,26 @@ namespace Osu.Music.Services.Audio
     public class AudioPlayback : BindableBase, IDisposable
     {
         #region Properties
-        public Beatmap Beatmap { get; set; }
-
-        private bool _mute = false;
-        public bool Mute
+        private Player _player;
+        public Player Player
         {
-            get => _mute;
-            set
-            {
-                SetProperty(ref _mute, value);
-                SetMute();
-            }
+            get => _player;
+            set => SetProperty(ref _player, value);
         }
 
-        private float _volume = 0.3f;
-        public float Volume
+        private IWavePlayer _device;
+        public IWavePlayer Device
         {
-            get => _volume;
-            set
-            {
-                SetProperty(ref _volume, Math.Min(Math.Max(value, 0), 1)); // Volume should be in range [0;1]
-                SetVolume();
-            }
+            get => _device;
+            set => SetProperty(ref _device, value);
         }
 
-        private bool _repeat;
-        public bool Repeat
+        private WaveStream _stream;
+        public WaveStream Stream
         {
-            get => _repeat;
-            set => SetProperty(ref _repeat, value);
+            get => _stream;
+            set => SetProperty(ref _stream, value);
         }
-
-        private bool _shuffle;
-        public bool Shuffle
-        {
-            get => _shuffle;
-            set => SetProperty(ref _shuffle, value);
-        }
-
-        public TimeSpan CurrentTime
-        {
-            get => fileStream != null ? fileStream.CurrentTime : TimeSpan.Zero;
-            set
-            {
-                if (fileStream != null)
-                    fileStream.CurrentTime = value;
-            }
-        }
-
-        public TimeSpan TotalTime => fileStream != null ? fileStream.TotalTime : TimeSpan.Zero;
-
-        public long Position
-        {
-            get => fileStream != null ? fileStream.Position : 0;
-            set
-            {
-                if (fileStream != null)
-                    fileStream.Position = value;
-            }
-        }
-        public long Length => fileStream != null ? fileStream.Length : 0;
-
-        public PlaybackState PlaybackState => playbackDevice != null ? playbackDevice.PlaybackState : PlaybackState.Stopped;
         #endregion
 
         #region Events
@@ -79,10 +37,10 @@ namespace Osu.Music.Services.Audio
         public event EventHandler<FftEventArgs> FftCalculated;
         #endregion
 
-        #region Private variables
-        private IWavePlayer playbackDevice;
-        private WaveStream fileStream;
-        #endregion
+        public AudioPlayback(Player player)
+        {
+            Player = player;
+        }
 
         public void Load()
         {
@@ -94,24 +52,24 @@ namespace Osu.Music.Services.Audio
 
         private void CloseFile()
         {
-            fileStream?.Close();
-            fileStream?.Dispose();
-            fileStream = null;
+            Stream?.Close();
+            Stream?.Dispose();
+            Stream = null;
         }
 
         private void OpenFile()
         {
             try
             {
-                AudioFileReader inputStream = new AudioFileReader(Beatmap.AudioFilePath);
-                fileStream = inputStream;
+                AudioFileReader inputStream = new AudioFileReader(Player.Beatmap.AudioFilePath);
+                Stream = inputStream;
 
                 SampleAggregator aggregator = new SampleAggregator(inputStream)
                 {
                     PerformFFT = true
                 };
                 aggregator.FftCalculated += (s, a) => FftCalculated?.Invoke(this, a);
-                playbackDevice.Init(aggregator);
+                Device.Init(aggregator);
             }
             catch
             {
@@ -121,10 +79,10 @@ namespace Osu.Music.Services.Audio
 
         private void EnsureDeviceCreated()
         {
-            if (playbackDevice != null)
+            if (Device != null)
             {
-                playbackDevice.Dispose();
-                playbackDevice = null;
+                Device.Dispose();
+                Device = null;
             }
 
             CreateDevice();
@@ -132,54 +90,54 @@ namespace Osu.Music.Services.Audio
 
         private void CreateDevice()
         {
-            playbackDevice = new WaveOut { DesiredLatency = 200, Volume = _mute ? 0 : _volume };
+            Device = new WaveOut { DesiredLatency = 200, Volume = Player.Mute ? 0 : Player.Volume };
 
-            playbackDevice.PlaybackStopped += (s, a) =>
+            Device.PlaybackStopped += (s, a) =>
             {
                 // I use difference between TimeSpans because some song are ending before EOF. On average it is less than 50 ms
                 // so window in 100 ms should do the work.
                 // Downside for this is that if someone will stop the song within 100 ms before it's end program will think that it should play
                 // next song. But it should occur extremly rarely. If someone will suggest better way to detect when song ended by itself
                 // I'll fix this semi hack.
-                if (a.Exception == null && fileStream != null && (fileStream.TotalTime - fileStream.CurrentTime).TotalMilliseconds < 100)
-                    BeatmapEnded?.Invoke(this, new BeatmapEventArgs(Beatmap));
+                if (a.Exception == null && Stream != null && (Stream.TotalTime - Stream.CurrentTime).TotalMilliseconds < 100)
+                    BeatmapEnded?.Invoke(this, new BeatmapEventArgs(Player.Beatmap));
             };
         }
 
-        private void SetMute()
+        public void SetMute()
         {
-            if (playbackDevice != null)
-                playbackDevice.Volume = _mute ? 0 : _volume;
+            if (Device != null)
+                Device.Volume = Player.Mute ? 0 : Player.Volume;
         }
 
-        private void SetVolume()
+        public void SetVolume()
         {
-            if (playbackDevice != null)
-                playbackDevice.Volume = _volume;
+            if (Device != null)
+                Device.Volume = Player.Volume;
         }
 
         public void Play()
         {
-            if (playbackDevice != null && fileStream != null && playbackDevice.PlaybackState != PlaybackState.Playing)
-                playbackDevice.Play();
+            if (Device != null && Stream != null && Device.PlaybackState != PlaybackState.Playing)
+                Device.Play();
         }
 
-        public void Pause() => playbackDevice?.Pause();
+        public void Pause() => Device?.Pause();
 
         public void Stop()
         {
-            playbackDevice?.Stop();
+            Device?.Stop();
 
-            if (fileStream != null)
-                fileStream.Position = 0;
+            if (Stream != null)
+                Stream.Position = 0;
         }
 
         public void Dispose()
         {
             Stop();
             CloseFile();
-            playbackDevice?.Dispose();
-            playbackDevice = null;
+            Device?.Dispose();
+            Device = null;
         }
     }
 }
---
+++ src/Osu.Music.Services/Audio/Player.cs
@@ -0,0 +1,100 @@
+﻿using NAudio.Wave;
+using Osu.Music.Common.Models;
+using Prism.Mvvm;
+using System;
+
+namespace Osu.Music.Services.Audio
+{
+    public class Player : BindableBase, IDisposable
+    {
+        private AudioPlayback _playback;
+        public AudioPlayback Playback
+        {
+            get => _playback;
+            set => SetProperty(ref _playback, value);
+        }
+
+        #region Properties
+        private Beatmap _beatmap;
+        public Beatmap Beatmap
+        {
+            get => _beatmap;
+            set => SetProperty(ref _beatmap, value);
+        }
+
+        private bool _mute = false;
+        public bool Mute
+        {
+            get => _mute;
+            set
+            {
+                SetProperty(ref _mute, value);
+                Playback.SetMute();
+            }
+        }
+
+        private float _volume = 0.3f;
+        public float Volume
+        {
+            get => _volume;
+            set
+            {
+                SetProperty(ref _volume, Math.Min(Math.Max(value, 0), 1)); // Volume should be in range [0;1]
+                Playback.SetVolume();
+            }
+        }
+
+        private bool _repeat;
+        public bool Repeat
+        {
+            get => _repeat;
+            set => SetProperty(ref _repeat, value);
+        }
+
+        private bool _shuffle;
+        public bool Shuffle
+        {
+            get => _shuffle;
+            set => SetProperty(ref _shuffle, value);
+        }
+
+        public TimeSpan CurrentTime
+        {
+            get => Playback.Stream?.CurrentTime ?? TimeSpan.Zero;
+            set
+            {
+                if (Playback.Stream != null)
+                    Playback.Stream.CurrentTime = value;
+            }
+        }
+
+        public TimeSpan TotalTime => Playback.Stream?.TotalTime ?? TimeSpan.Zero;
+
+        public long Position
+        {
+            get => Playback.Stream?.Position ?? 0;
+            set
+            {
+                if (Playback.Stream != null)
+                    Playback.Stream.Position = value;
+            }
+        }
+        public long Length => Playback.Stream?.Length ?? 0;
+
+        public PlaybackState PlaybackState => Playback.Device?.PlaybackState ?? PlaybackState.Stopped;
+        #endregion
+
+        public Player()
+        {
+            Mute = false;
+            Repeat = false;
+            Shuffle = false;
+            Volume = 0.3f;
+        }
+
+        public void Dispose()
+        {
+            Playback?.Dispose();
+        }
+    }
+}
