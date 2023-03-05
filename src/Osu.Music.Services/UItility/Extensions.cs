906967c23aeb91073c396851c801420302bc8e6c
Laritello <larik97@gmail.com>
2023-03-05 03:03:08 +0300

    Fixed border color

--- src/Osu.Music.Services/UItility/Extensions.cs
+++ src/Osu.Music.Services/UItility/Extensions.cs
@@ -40,23 +40,14 @@ namespace Osu.Music.Services.UItility
                 G = (byte)Math.Floor(0.75 * mainColor.G),
                 B = (byte)Math.Floor(0.75 * mainColor.B)
             };
-            Color borderColor = new Color()
-            {
-                A = 64,
-                R = mainColor.R,
-                G = mainColor.G,
-                B = mainColor.B
-            };
 
             dictionary["ColorMain"] = mainColor;
             dictionary["ColorMainLight"] = lightColor;
             dictionary["ColorMainDark"] = darkColor;
-            dictionary["ColorMainBorder"] = borderColor;
 
             dictionary["SolidColorBrushMain"] = new SolidColorBrush(mainColor);
             dictionary["SolidColorBrushMainLight"] = new SolidColorBrush(lightColor);
             dictionary["SolidColorBrushMainDark"] = new SolidColorBrush(darkColor);
-            dictionary["SolidColorBrushMainBorder"] = new SolidColorBrush(borderColor);
 
             CustomColorTheme theme = collection[1] as CustomColorTheme;
             theme.PrimaryColor = mainColor;
